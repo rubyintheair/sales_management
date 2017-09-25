af911b2d0b4ee13149785eb55a1bdf08f11c91a5
Quy <quy.nguyenngoctp@gmail.com>
2017-09-25 23:34:33 +0700

    add has_secure_password for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save 
+      flash[:success] = "User created."
+    else 
+      flash[:error] = "#{@user.errors.full_messages.to_sentence}"
+    end 
+    redirect_to users_new_path
+  end 
+
+  def user_params
+    params.require(:user).permit(:code, :name, :email, :password, :password_confirmation)
+  end 
 end
